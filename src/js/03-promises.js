ebdaa39fff28f9fc496e8465bbed85df308b85dc
eduard-kaluhin <renoserviser@gmail.com>
2023-06-07 22:56:27 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,16 @@
 import Notiflix from 'notiflix';
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
         resolve({ position, delay });
-      }, delay);
-    });
-  } else {
-    return new Promise((_, reject) => {
-      setTimeout(() => {
+      } else {
         reject({ position, delay });
-      }, delay);
-    });
-  }
+      }
+    }, delay);
+  });
 }
 
 const form = document.querySelector('.form');
